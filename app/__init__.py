f79398f6247abb1497000a16b05777f6e877b56d
ihoochie <ilya_andrutsky@mail.ru>
2020-01-01 01:56:40 +0300

    ass password reset tokens

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -16,6 +17,8 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 login.login_message = 'Введите логин и пароль, чтобы войти'
+
+mail = Mail(app)
 
 
 if not app.debug:
