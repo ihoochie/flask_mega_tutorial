ac26febc2191d1861adda210732ed39c60561dc3
ihoochie <ilya_andrutsky@mail.ru>
2019-12-31 17:03:26 +0300

    pagination

--- config.py
+++ config.py
@@ -14,4 +14,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['ilya_andrutsky@mail.ru']
+    ADMINS = ['ilya@example.ru']
+
+    POSTS_PER_PAGE = 25
