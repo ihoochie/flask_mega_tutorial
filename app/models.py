23a2f35538f8836f6b8534cdff6cdad88d1e15cf
ihoochie <ilya_andrutsky@mail.ru>
2020-01-13 00:17:18 +0300

    email reset

--- app/models.py
+++ app/models.py
@@ -68,7 +68,7 @@ class User(UserMixin, db.Model):
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY']
+            id = jwt.decode(token, app.config['SECRET_KEY'],
                 algorithms=['HS256'])['reset_password']
         except:
             return
