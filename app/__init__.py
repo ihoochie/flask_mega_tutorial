7972030da25d1301dfbad153896475862f74ab3d
ihoochie <ilya_andrutsky@mail.ru>
2020-01-25 17:38:19 +0300

    prettify front with bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -19,6 +20,8 @@ login.login_view = 'login'
 login.login_message = 'Введите логин и пароль, чтобы войти'
 
 mail = Mail(app)
+
+bootstrap = Bootstrap(app)
 
 
 if not app.debug:
