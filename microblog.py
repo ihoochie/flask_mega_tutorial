7e16180c03472b124327d229524ccd2fef68e355
Ilya Andrutskiy <50034902+ihoochie@users.noreply.github.com>
2020-04-19 20:16:44 +0300

    ch21 notifications (#16)

--- microblog.py
+++ microblog.py
@@ -1,9 +1,11 @@
 from app import create_app, db, cli
-from app.models import User, Post, SearchableMixin
+from app.models import User, Post, SearchableMixin, Notification, \
+    Message
 
 app = create_app()
 cli.register(app)
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'SearchableMixin': SearchableMixin}
+    return {'db': db, 'User': User, 'Post': Post, 'SearchableMixin': SearchableMixin,
+            'Message': Message, 'Notification': Notification}
